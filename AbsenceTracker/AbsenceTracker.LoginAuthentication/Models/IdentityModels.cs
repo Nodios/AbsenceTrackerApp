1b97562a81239bfdbce502d2a49fdbe678966de8
Stjepan Baricevic <Stjepan Baricevic>
2016-11-17 16:06:09 +0100

    Database models added

--- AbsenceTracker/AbsenceTracker.LoginAuthentication/Models/IdentityModels.cs
+++ AbsenceTracker/AbsenceTracker.LoginAuthentication/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace AbsenceTracker.LoginAuthentication.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("name=AbsenceTrackerSbConnString", throwIfV1Schema: false)
         {
         }
 
