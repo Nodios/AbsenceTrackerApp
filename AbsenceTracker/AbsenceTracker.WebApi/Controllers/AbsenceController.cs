9c31db0f980299c098f93461b45889582006e229
Josip Tomaić <Josip Tomai>
2016-11-21 16:08:04 +0100

    Added Controllers for Absence, Sickness and Vacation

--- AbsenceTracker/AbsenceTracker.WebApi/Controllers/AbsenceController.cs
+++ AbsenceTracker/AbsenceTracker.WebApi/Controllers/AbsenceController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using AbsenceTracker.Model.Common.IDomainModels;
+using AbsenceTracker.Model.DomainModels;
+using AbsenceTracker.Service.Common;
+using AbsenceTracker.WebApi.ViewModels;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace AbsenceTracker.WebApi.Controllers
@@ -10,5 +16,138 @@ namespace AbsenceTracker.WebApi.Controllers
     [RoutePrefix("api/absence")]
     public class AbsenceController : ApiController
     {
+        protected IAbsenceService AbsenceService;
+
+        public AbsenceController(IAbsenceService service)
+        {
+            this.AbsenceService = service;
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        public async Task<HttpResponseMessage> GetAll()
+        {
+            try
+            {
+                var response = Mapper.Map<IEnumerable<AbsenceView>>(await AbsenceService.ReadAll());
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public async Task<HttpResponseMessage> Get(string id)
+        {
+            try
+            {
+                var response = AutoMapper.Mapper.Map<AbsenceView>(await AbsenceService.Read(id));
+
+                if (response == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid id.");
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<HttpResponseMessage> Add(AbsenceView absenceView)
+        {
+            int counter = 0;
+            try
+            {
+                if(absenceView.Sickness != null)
+                {
+                    counter++;
+                }
+                if(absenceView.Vacation != null)
+                {
+                    counter++;
+                }
+                if(absenceView.Compensation != null)
+                {
+                    counter++;
+                }
+                if (absenceView.Type == null || (counter == 0 || counter > 1))
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid request");
+
+                absenceView.Id = Guid.NewGuid().ToString();
+
+                var response = await AbsenceService.Add(Mapper.Map<AbsenceDomain>(absenceView));
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public async Task<HttpResponseMessage> Delete(string id)
+        {
+            try
+            {
+                var response = await AbsenceService.Delete(id);
+
+                if (response == 0)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Couldn't delete entry.");
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public async Task<HttpResponseMessage> Update(AbsenceView absenceView)
+        {
+            int counter = 0;
+            try
+            {
+                var toBeUpdated = await AbsenceService.Read(absenceView.Id);
+
+                if (absenceView.Sickness != null)
+                {
+                    counter++;
+                    toBeUpdated.Sickness = Mapper.Map<ISicknessDomain>(absenceView.Sickness);
+                }
+                if (absenceView.Vacation != null)
+                {
+                    counter++;
+                    toBeUpdated.Vacation = Mapper.Map<IVacationDomain>(absenceView.Vacation);
+                }
+                if (absenceView.Compensation != null)
+                {
+                    counter++;
+                    toBeUpdated.Compensation = Mapper.Map<ICompensationDomain>(absenceView.Compensation);
+                }
+
+                if (toBeUpdated == null || (counter == 0 || counter > 1))
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Entry not found");
+
+                var response = await AbsenceService.Update(Mapper.Map<AbsenceDomain>(toBeUpdated));
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
     }
 }
--- AbsenceTracker/AbsenceTracker.WebApi/Controllers/VacationController.cs
+++ AbsenceTracker/AbsenceTracker.WebApi/Controllers/VacationController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using AbsenceTracker.Model.Common.IDomainModels;
+using AbsenceTracker.Model.DomainModels;
+using AbsenceTracker.Service.Common;
+using AbsenceTracker.WebApi.ViewModels;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace AbsenceTracker.WebApi.Controllers
@@ -10,5 +16,125 @@ namespace AbsenceTracker.WebApi.Controllers
     [RoutePrefix("api/vacation")]
     public class VacationController : ApiController
     {
+        protected IVacationService VacationService;
+
+        public VacationController(IVacationService service)
+        {
+            this.VacationService = service;
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        public async Task<HttpResponseMessage> GetAll()
+        {
+            try
+            {
+                var response = Mapper.Map<IEnumerable<VacationView>>(await VacationService.ReadAll());
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public async Task<HttpResponseMessage> Get(string id)
+        {
+            try
+            {
+                var response = AutoMapper.Mapper.Map<VacationView>(await VacationService.Read(id));
+
+                if (response == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid id.");
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<HttpResponseMessage> Add(VacationView vacationView)
+        {
+            try
+            {
+                if (vacationView.Absence == null || vacationView.StartDate == null || vacationView.EndDate == null || vacationView.Time == 0 || vacationView.TimeLeft == 0)
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid request");
+
+                vacationView.Id = Guid.NewGuid().ToString();
+
+                var response = await VacationService.Add(Mapper.Map<VacationDomain>(vacationView));
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public async Task<HttpResponseMessage> Delete(string id)
+        {
+            try
+            {
+                var response = await VacationService.Delete(id);
+
+                if (response == 0)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Couldn't delete entry.");
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public async Task<HttpResponseMessage> Update(VacationView vacationView)
+        {
+            try
+            {
+                var toBeUpdated = await VacationService.Read(vacationView.Id);
+
+                if (toBeUpdated == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Entry not found");
+
+                if (vacationView.StartDate != null)
+                {
+                    toBeUpdated.StartDate = vacationView.StartDate;
+                }
+                if (vacationView.EndDate != null)
+                {
+                    toBeUpdated.EndDate = vacationView.EndDate;
+                }
+                if (vacationView.Time != 0)
+                {
+                    toBeUpdated.Time = vacationView.Time;
+                }
+                if(vacationView.TimeLeft != 0)
+                {
+                    toBeUpdated.TimeLeft = vacationView.TimeLeft;
+                }
+
+                var response = await VacationService.Update(Mapper.Map<VacationDomain>(toBeUpdated));
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
     }
 }
