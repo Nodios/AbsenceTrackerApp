01879ca2f9b8613219b8cd55d3682e4a994130ad
Your Name <you@example.com>
2016-11-21 16:04:22 +0100

    add AspNetUserRoleView

---
+++ AbsenceTracker/AbsenceTracker.WebApi/ViewModels/AspNetUserRoleView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AbsenceTracker.WebApi.ViewModels
+{
+    public class AspNetUserRoleView
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<AspNetUserView> AspNetUsers { get; set; }
+    }
+}
